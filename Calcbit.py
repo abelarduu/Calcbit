d6129cd7365862343eec0c1ed8ce7534251665d6
Abel Lucas <abelucas009@gmail.com>
2023-03-08 10:33:22 -0300

    Initial commit

---
+++ Calcbit.py
@@ -0,0 +1,100 @@
+#############################
+#CalcBit - calculadora pixel#
+#############################
+#Importações necessárias
+import pyxel
+#Botões
+class Button(object):
+    def __init__(self,x:int,y:int,imgx:int,imgy:int, width:int, height:int, key:None, value:str):
+
+        self.x, self.y=x,y
+        self.img,self.imgx, self.imgy=0,imgx, imgy
+        self.width, self.height=width, height
+        self.value= value
+        self.key= key
+        
+    def press(self):
+        if pyxel.mouse_y > self.y+2 and pyxel.mouse_y < self.y +24 or pyxel.btnp(self.key):
+            if pyxel.mouse_x > self.x and pyxel.mouse_x < self.x +23 or pyxel.btnp(self.key):
+                if pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) or pyxel.btnp(self.key):
+                    self.img=1
+                    return True
+
+        if pyxel.btnr(pyxel.MOUSE_BUTTON_LEFT) or pyxel.btnr(self.key):
+            self.img=0
+
+    def draw(self):
+        pyxel.blt(self.x, self.y,self.img, self.imgx, self.imgy, 24, 24)
+
+#Calculadora
+class Calc:
+    def __init__(self):
+        #Declarando variaveis
+        self.result= ""
+        #Numeros
+        self.btn0=Button(x=0,y=121,imgx=0,imgy=96, width=24, height=24, key= pyxel.KEY_KP_0, value= '0')
+        self.btn1=Button(x=0,y=97,imgx=0,imgy=72, width=24, height=24, key= pyxel.KEY_KP_1, value= '1')
+        self.btn2=Button(x=24,y=97,imgx=24,imgy=72, width=24, height=24, key= pyxel.KEY_KP_2, value= '2')
+        self.btn3=Button(x=48,y=97,imgx=48,imgy=72, width=24, height=24, key= pyxel.KEY_KP_3, value= '3')
+        self.btn4=Button(x=0,y=73,imgx=0,imgy=48, width=24, height=24, key= pyxel.KEY_KP_4, value= '4')
+        self.btn5=Button(x=24,y=73,imgx=24,imgy=48, width=24, height=24, key= pyxel.KEY_KP_5, value= '5')
+        self.btn6=Button(x=48,y=73,imgx=48,imgy=48, width=24, height=24, key= pyxel.KEY_KP_6, value= '6')
+        self.btn7=Button(x=0,y=49,imgx=0,imgy=24, width=24, height=24, key= pyxel.KEY_KP_7, value= '7')
+        self.btn8=Button(x=24,y=49,imgx=24,imgy=24, width=24, height=24, key= pyxel.KEY_KP_8, value= '8')
+        self.btn9=Button(x=48,y=49,imgx=48,imgy=24, width=24, height=24, key= pyxel.KEY_KP_9, value= '9')
+        #Simbolos/operadores
+        self.btnAC=Button(x=72,y=25,imgx=72,imgy=0, width=24, height=24, key= pyxel.KEY_DELETE, value= '')
+        self.btnEqual=Button(x=48,y=121,imgx=48,imgy=96, width=24, height=24, key= pyxel.KEY_KP_ENTER, value= '')
+        self.btnAdd=Button(x=72,y=121,imgx=72,imgy=96, width=24, height=24, key= pyxel.KEY_KP_PLUS, value= '+')
+        self.btnSub=Button(x=72,y=97,imgx=72,imgy=72, width=24, height=24, key= pyxel.KEY_KP_MINUS, value= '-')
+        self.btnMult=Button(x=72,y=73,imgx=72,imgy=48, width=24, height=24, key= pyxel.KEY_KP_MULTIPLY, value= '*')
+        self.btnDiv=Button(x=72,y=49,imgx=72,imgy=24, width=24, height=24, key= pyxel.KEY_KP_DIVIDE, value= '/')
+        self.btnPerc=Button(x=48,y=25,imgx=48,imgy=0, width=24, height=24, key= pyxel.KEY_PERCENT, value= '%')
+        self.btnPoint=Button(x=24,y=121,imgx=24,imgy=96, width=24, height=24, key= pyxel.KEY_KP_PERIOD , value= '.')
+        self.btnParentheses1=Button(x=0,y=25,imgx=0,imgy=0, width=24, height=24, key= pyxel.KEY_LEFTPAREN, value= '(')
+        self.btnParentheses2=Button(x=24,y=25,imgx=24,imgy=0, width=24, height=24, key= pyxel.KEY_RIGHTPAREN , value= ')')
+        #Lista de Botões
+        self.btnList=[self.btn0, self.btn1, self.btn2, self.btn3, self.btn4, self.btn5, self.btn6, self.btn7, self.btn8, self.btn9,
+                      self.btnAC, self.btnEqual, self.btnAdd, self.btnSub, self.btnMult, self.btnDiv, self.btnPerc, self.btnPoint, self.btnParentheses1, self.btnParentheses2]
+        pyxel.init(96,145, 'CalcBit',quit_key= False)
+        pyxel.load('resources/CalcBit.pyxres')
+        pyxel.run(self.update, self.draw)
+        
+    #Método de verificação de interação a cada quadro
+    def update(self):
+        print(f"{self.result}")
+        for btn in self.btnList:
+            if btn.press():
+                #Se num1 receber o num maximo
+                if len(str(self.result)) < 21:
+                    self.result =str(self.result)+str(btn.value)
+                #Botão AC
+                if btn.key== pyxel.KEY_DELETE:      #if for Butão AC:
+                    self.result= ""                 #   apague tudo
+                #Botão Igual
+                if btn.key== pyxel.KEY_KP_ENTER and len(self.result)!= 0:
+                    self.getResult()
+        #Botão BACKSPACE
+        if pyxel.btnp(pyxel.KEY_BACKSPACE) and len(self.result) > 0:            #if for Butão AC:
+            self.result= self.result.replace(str(self.result[-1]), "",1)        #   apague ultimo caractere
+
+    #Método para atualização de tela a cada quadro
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.mouse(visible= True)
+        #Display da calculadora
+        #Tela de saida de dados da calculadora         
+        pyxel.rect(5,2,86,20,11)
+        pyxel.rectb(5,2,86,20,3)
+        pyxel.text(90-len(str(self.result))*4,15,str(self.result),7)
+        for btn in self.btnList:
+            btn.draw()
+
+    #Método para receber o resultado do calculo inserido
+    def getResult(self):
+        self.result= eval(self.result)
+##########################################
+#verificação da execução direta do módulo#
+##########################################
+if __name__== "__main__":
+    Calc()
---
+++ README.md
@@ -0,0 +1,45 @@
+<div align="center">
+<div>
+<img src="img/calcbit icon.png" alt="Calcbit Icon">
+
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?font=Terminus&pause=100&center=verdadeiro&vCenter=falso&repeat=verdadeiro&width=230&lines=Calculadora+Simples)](https://git.io/typing-svg)
+
+</div>
+
+<div>
+  <h4>🚧⚠️Em desenvolvimento...⚠️🚧</h4>
+  <figure>
+    <img src="img/interface.png" alt="Interface do Calcbit"></br>
+    <figcaption>Aba/Interface do Calcbit</figcaption>
+  </figure>
+</div>
+
+<div>
+  <h3>Sobre o projeto</h3>
+  <p>Calcbit é uma calculadora simples com o ambiente gráfico no estilo de pixel art, com seu sistema feito em python utilizando o módulo pyxel.</p>
+  <p>
+    <a href="https://www.python.org/">
+      <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python V.3.9" ></a>
+    <a href="https://github.com/kitao/pyxel">
+      <img src="https://img.shields.io/badge/Pyxel-v1.9.12-blue?style=for-the-badge&logo=python&logoColor=white" alt="Pyxel V.1.9.12" ></a>
+  </p>
+  <!--<p>
+    <img src="/github/pipenv/locked/python-version/:user/:repo">
+    ![example branch parameter](https://github.com//github/pipenv/locked/python-version/:user/:repo)
+    Tagpyxel / version
+  </p>
+  
+  <div style="display: flex, align-items: center">
+      <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white">
+      <img src="https://img.shields.io/badge/Itch.io-FA5C5C?style=for-the-badge&logo=itchdotio&logoColor=white">
+      <img alt="GitHub top language" src="https://img.shields.io/github/languages/top/abelarduu/Projects?style=for-the-badge">
+      <img alt="GitHub Pipenv locked Python version" src="https://img.shields.io/github/pipenv/locked/python-version/abelarduu/Projects?style=for-the-badge">
+    ![image](https://img.shields.io/badge/Notepad++-90E59A.svg?style=for-the-badge&logo=notepad%2B%2B&logoColor=black)
+  </div>-->
+
+<div>
+  <h3>Resultado</h3>
+  <img src="img/CalcBit.gif" alt="Calcbit gif">
+</div>
+</div>
+</div>
